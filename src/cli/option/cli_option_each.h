cc7668d5865f81d319e89f4a7fb39e0a95e3a846
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-01 13:16:14 +0330

    feat: add CLIOptionEach class

---
+++ src/cli/option/cli_option_each.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "cli_option.h"
+
+namespace colosseum {
+
+class CLIOptionEach : public CLIOption {
+public:
+    void parse(const std::vector<std::string>& args) override {
+        std::cout << "CLIOptionEach parse called" << std::endl;
+    }
+};
+
+} // namspace colosseum
