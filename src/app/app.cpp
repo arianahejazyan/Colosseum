8c9d03282f5d6ab902cd2cd0b7ef9035d671c9c4
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-01 13:12:59 +0330

    feat: add App class

---
+++ src/app/app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "app.h"
+
+namespace colosseum {
+
+int App::run(int argc, char const* argv[]) {
+    std::cout << "Feature coming soon in future releases." << std::endl;
+    return 0;
+}
+
+} // namspace colosseum
---
+++ src/app/app.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace colosseum {
+
+class App {
+public:
+    // Main entry point
+    int run(int argc, char const* argv[]);
+};
+
+} // namspace colosseum
