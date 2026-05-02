18e3fb49c24153f125441ca9521f3f7bee3a42a6
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-02 15:45:31 +0330

    feat: implement CLIOptionEngine parse_pairs method

--- src/cli/option/cli_option_engine.h
+++ src/cli/option/cli_option_engine.h
@@ -1,13 +1,29 @@
 #pragma once
 
 #include "cli_option.h"
+#include "exception.h"
 
 namespace colosseum {
 
 class CLIOptionEngine : public CLIOptionPair {
 protected:
     void parse_pairs(const std::vector<Pair>& pairs, CLIConfig& config) override {
-        std::cout << "CLIOptionEngine parse_pairs called" << std::endl;
+
+        auto& engine = config.engine_configs.emplace_back();
+
+        for (const auto& [key, value]: pairs) {
+
+            if (key.starts_with("option.")) {
+                engine.options.emplace_back(key.substr(7), value);
+            }
+
+            else if (key == "name") { engine.name = value; }
+            else if (key == "cmd" ) { engine.cmd  = value; }
+            else if (key == "dir" ) { engine.dir  = value; }
+            else if (key == "args") { engine.args = value; }
+
+            else throw colosseum_exception("Unknown engine option key: " + key);
+        }
     }
 };
 
