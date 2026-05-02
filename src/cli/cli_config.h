f98a3816a79a8b1843f883ae67e6af383d4b18a5
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-02 14:57:22 +0330

    feat: addengine configs to CLIConfig

--- src/cli/cli_config.h
+++ src/cli/cli_config.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <vector>
+#include "engine/engine_config.h"
+
 namespace colosseum {
 
 struct CLIConfig {
 public:
+    std::vector<EngineConfig> engine_configs;
 };
 
 } // namspace colosseum
