51485a6688aeab71bbadb6de8f97b549183b7ecf
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-04 16:29:17 +0330

    feat: add TournamentConfig to CLIConfig

--- src/cli/cli_config.h
+++ src/cli/cli_config.h
@@ -2,11 +2,13 @@
 
 #include <vector>
 #include "engine/engine_config.h"
+#include "tournament/tournament_config.h"
 
 namespace colosseum {
 
 struct CLIConfig {
 public:
+    TournamentConfig tournament;
     std::vector<EngineConfig> engine_configs;
 };
 
