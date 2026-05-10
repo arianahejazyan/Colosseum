b78aef2a3b36e0ca1ca560f803b51e2a804fac60
ariana hejazyan <arianahejazyan@gmail.com>
2026-05-10 14:39:30 +0330

    feat: add OptionEngine class

---
+++ src/cli/option/option_engine.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "engine/config/config.h"
+#include "option/base/option_key_value.h"
+#include "exception.h"
+
+namespace colosseum::cli {
+
+class OptionEngine : public OptionKeyValue {
+public:
+    // Default constructor
+    OptionEngine(const std::string& str) : OptionKeyValue(str) {}
+
+protected:
+    void parse(const std::vector<KeyValue>& key_values, cli::Config& config) const override {
+        auto& engine = config.engine.emplace_back();
+        parse_engine_options(key_values, engine);
+    }
+};
+
+} // namspace colosseum
